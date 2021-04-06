428cd24815eef1544ab518dcadf834b23586b5fc
Konstatin Bogdanov <>
2021-04-06 07:47:39 +0300

    Добавлены протоколы

---
+++ DatabaseAbstraction/DatabaseServiceProtocol.swift
@@ -0,0 +1,32 @@
+//
+//  DatabaseServiceProtocol.swift
+//  DatabaseAbstraction
+//
+//  Created by Константин Богданов on 05.04.2021.
+//
+
+import CoreData
+
+/// Протокол сервиса общающегося с БД
+public protocol DatabaseServiceProtocol {
+
+	/// Извлекает модели из БД
+	/// - Parameter convertClosure: блок для преобразования managed модели в обычную
+	func fetch<Model>(convertClosure: (StoredObjectProtocol) -> Model?) -> [Model]
+
+	/// Извлекает модели из БД
+	/// - Parameters:
+	///   - convertClosure: блок для преобразования managed модели в обычную
+	///   - predicate: предикат для выборки моделей
+	func fetch<Model>(convertClosure: (StoredObjectProtocol) -> Model?,
+													predicate: [String: String]) -> [Model]
+
+	/// Добавить модели в БД
+	/// - Parameters:
+	///   - models: модели
+	///   - convertClosure: блок, в котором происходит установка свойст managed модели
+	///   - completion: блок, выполняющийся по завершению сохранения моделей
+	func insert<Model>(models: [Model],
+												convertClosure: @escaping (Model, StoredObjectProtocol) -> Void,
+												completion: @escaping () -> Void)
+}
---
+++ DatabaseAbstraction/StoredObjectProtocol.swift
@@ -0,0 +1,23 @@
+//
+//  StoredObjectProtocol.swift
+//  DatabaseAbstraction
+//
+//  Created by Константин Богданов on 05.04.2021.
+//
+
+/// Протокол объекта, хранимого в БД
+public protocol StoredObjectProtocol {
+
+	/// Идентификатор объекта в БД
+	static var objectIdentifier: String { get }
+
+	///  Устанавливает значение в объект
+	/// - Parameters:
+	///   - value: значение
+	///   - key: ключ
+	func setValue<Value>(_ value: Value?, forKey key: String)
+
+	/// Получает значение по ключу
+	/// - Parameter key: ключ
+	func value<Value>(forKey key: String) -> Value?
+}
